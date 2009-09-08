c672c4621c5a5d122763bc12e88c5c7bb7082dce
John Van Enk <vanenkj@gmail.com>
2009-09-08 18:33:02 -0400

    Fixed cabal package and added catchall module.

---
+++ src/Language/Gator.hs
@@ -0,0 +1,13 @@
+module Language.Gator (
+    module Language.Gator.General,
+    module Language.Gator.IO,
+    module Language.Gator.Logic,
+    module Language.Gator.Ops,
+    module Language.Gator.Gates,
+) where
+
+import Language.Gator.General
+import Language.Gator.IO
+import Language.Gator.Logic
+import Language.Gator.Ops
+import Language.Gator.Gates
