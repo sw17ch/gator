6c5a6a58a7fea706e61892daed7c2e66b777ccb8
John Van Enk <john@dorner-john-linux.(none)>
2009-09-08 17:23:56 -0400

    Added example output and cleaned up a little.

--- main.hs
+++ main.hs
@@ -14,10 +14,25 @@ main = do
     print $ compile gates
 
 gates = do
-    newOr "or10"
     o <- doOr in0 in1 "or0"
     lineTo o out0 "ln0"
     where
         in0  = Input "in0"
         in1  = Input "in1"
         out0 = Output "out0"
+
+    {- Produces:
+     -  Logic {gateSets = GateSets {
+     -          orGates = fromList [OrGate "or0"],
+     -          andGates = fromList [],
+     -          traces = fromList [Line "ln0"],
+     -          inputs = fromList [],
+     -          outputs = fromList []
+     -      },
+     -      joints = fromList [
+     -          ("in0.out","or0.in0"),
+     -          ("in1.out","or0.in1"),
+     -          ("or0.out","out0.in0")
+     -      ]
+     -  }
+     -}
