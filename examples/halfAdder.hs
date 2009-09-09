84917ae90209c888406c0d4cc6e6bbbc67cdab05
John Van Enk <john@dorner-john-linux.(none)>
2009-09-09 11:36:35 -0400

    Updated to make the half adder a little more modular.

--- examples/halfAdder.hs
+++ examples/halfAdder.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Main where
 
 import Language.Gator
@@ -5,22 +6,27 @@ import Control.Monad.State
 
 main :: IO ()
 main = do
-    l <- compile halfAdder
+    l <- compile logic
     print l
 
-{-
- - A Half Adder.
- - See: http://en.wikipedia.org/wiki/Adder_(electronics)#Half_adder
- -}
-halfAdder :: StateT Logic IO ()
-halfAdder = do
+logic :: StateT Logic IO ()
+logic = do
     inA <- newInput "A"
     inB <- newInput "B"
     outS <- newOutput "S"
     outC <- newOutput "C"
 
-    xor <- doXOr inA inB "xor0"
-    and <- doAnd inA inB "and0"
+    (xor,and) <- halfAdder inA inB
 
     connect xor outS
     connect and outC
+
+{-
+ - A Half Adder.
+ - See: http://en.wikipedia.org/wiki/Adder_(electronics)#Half_adder
+ -}
+halfAdder :: (Out a, Out b, MonadState Logic m) => a -> b -> m (XOrGate, AndGate)
+halfAdder inA inB = do
+    xor <- doXOr inA inB "xor0"
+    and <- doAnd inA inB "and0"
+    return (xor,and)
